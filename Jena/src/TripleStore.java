70991ddf1da7b77af93d58e9954f732ec039eace
Dsprtn <drtsprtn@gmail.com>
2015-11-26 09:47:37 +0100

    Test

--- Jena/src/TripleStore.java
+++ Jena/src/TripleStore.java
@@ -7,7 +7,7 @@ import javax.swing.table.DefaultTableModel;
 
 public class TripleStore {
 	
-	
+	// COPIED FROM BRENT :^)
 	
 	public static void main(String[] args) {
 		TripleStore store = new TripleStore();
